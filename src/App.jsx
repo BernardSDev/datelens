13c8b162b3f1b6e17e1a93e2cb2e3cc5bfc9bc34
bernardsam-apoh <sb.bernardsam@gmail.com>
2024-07-09 02:19:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="container bg-green-600 p-4">
+      <h1 className="bg-red-900 text-6xl font-bold underline">Datelens</h1>
+    </div>
+  );
+}
